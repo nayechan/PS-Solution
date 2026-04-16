29de2170a568930c75faf84c0acdc8d85f719511
nayechan <nayechan1121@gmail.com>
2026-04-16 22:09:10 +0900

    [Bronze V] Title: M-Climb, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ BOJ/34411/M－Climb.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	cout << a * b << "\n";
+
+	return 0;
+}
