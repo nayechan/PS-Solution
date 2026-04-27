f79b836959396d70070777aec86c53025bf5560d
nayechan <nayechan1121@gmail.com>
2026-04-27 21:56:12 +0900

    [level 3] Title: 이중우선순위큐, Time: 68.66 ms, Memory: 12.4 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
@@ -2,58 +2,100 @@
 #include <queue>
 #include <sstream>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
+
+constexpr int MAX_N = (1 << 20);
+int fenwick[MAX_N + 1] = {0, };
+
+void update(int idx, int value)
+{
+	while (idx <= MAX_N)
+	{
+		fenwick[idx] += value;
+		idx += idx & (-idx);
+	}
+}
+
+int query(int idx)
+{
+	int value = 0;
+	while (idx)
+	{
+		value += fenwick[idx];
+		idx &= (idx - 1);
+	}
+
+	return value;
+}
+
+int find_kth(int k, int sz) {
+    int idx = 0;
+    for (int i = MAX_N; i > 0; i >>= 1) {
+        if (idx + i <= sz && fenwick[idx + i] < k) {
+            idx += i;
+            k -= fenwick[idx];
+        }
+    }
+    return idx + 1;
+}
 
 vector<int> solution(vector<string> operations) {
     priority_queue<int, vector<int>, greater<int>> minQ, deletedMin;
     priority_queue<int, vector<int>, less<int>> maxQ, deletedMax;
     
-    for(string & op : operations)
+    vector<pair<int,int>> op;
+    vector<int> nums;
+    
+    for(string & _op : operations)
     {
-        stringstream ss(op);
-        char _op;
+        stringstream ss(_op);
+        
+        char __op;
         int v;
-        ss >> _op >> v;
-        if(_op == 'I')
+        
+        ss >> __op >> v;
+        op.push_back({__op == 'I', v});
+        if(__op == 'I') nums.push_back(v);
+    }
+    
+    sort(nums.begin(), nums.end());
+    nums.erase(unique(nums.begin(), nums.end()), nums.end());
+    
+    int sz = 0;
+    
+    for(auto& [_op, v] : op)
+    {
+        if(_op)
         {
-            minQ.push(v);
-            maxQ.push(v);
+            int idx = lower_bound(nums.begin(),nums.end(),v)-nums.begin();
+            update(idx+1, 1);
+            ++sz;
         }
         else
         {
-            if(v == 1)
+            if(sz == 0)
             {
-                if(maxQ.empty()) continue;
-                int target = maxQ.top();
-                maxQ.pop();
-                deletedMin.push(target);
-                //cout << target << "\n";
+                continue;
+            }
+            if(v == -1)
+            {
+                int idx = find_kth(1, nums.size());
+                update(idx, -1);
+                --sz;
             }
             else
             {
-                if(minQ.empty()) continue;
-                int target = minQ.top();
-                minQ.pop();
-                deletedMax.push(target);
-                //cout << target << "\n";
-            }
-            
-            while(!minQ.empty() && !deletedMin.empty() && minQ.top() == deletedMin.top())
-            {
-                minQ.pop();
-                deletedMin.pop();
-            }
-            
-            while(!maxQ.empty() && !deletedMax.empty() && maxQ.top() == deletedMax.top())
-            {
-                maxQ.pop();
-                deletedMax.pop();
+                int idx = find_kth(sz, nums.size());
+                update(idx, -1);
+                --sz;
             }
         }
     }
+    
     vector<int> answer(2, 0);
-    answer[0] = maxQ.empty() ? 0 : maxQ.top();
-    answer[1] = minQ.empty() ? 0 : minQ.top();
+    answer[0] = sz==0 ? 0 : nums[find_kth(sz, nums.size())-1];
+    answer[1] = sz==0 ? 0 : nums[find_kth(1, nums.size())-1];
     return answer;
 }
